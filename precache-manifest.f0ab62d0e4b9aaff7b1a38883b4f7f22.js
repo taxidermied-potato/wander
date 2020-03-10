8fe11682dd904546101ca835021e65d6cac75c5e
Al Yin <altyin@gmail.com>
2020-03-10 19:41:29 -0400

    Updates

---
+++ precache-manifest.f0ab62d0e4b9aaff7b1a38883b4f7f22.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eedce80e5e91fc6fab8d686082a7033",
+    "url": "/wander/index.html"
+  },
+  {
+    "revision": "1b4d27ee702a21846c3d",
+    "url": "/wander/static/css/main.3535f686.chunk.css"
+  },
+  {
+    "revision": "af47e0558615fd45a520",
+    "url": "/wander/static/js/2.3ee20663.chunk.js"
+  },
+  {
+    "revision": "7f3d623d03408f9350c3b5e194511d01",
+    "url": "/wander/static/js/2.3ee20663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b4d27ee702a21846c3d",
+    "url": "/wander/static/js/main.319d77f9.chunk.js"
+  },
+  {
+    "revision": "c5b655157dce2831edcf",
+    "url": "/wander/static/js/runtime-main.f86d4f6e.js"
+  },
+  {
+    "revision": "8db746ea0f28bdd22eae8b3ee10ac16a",
+    "url": "/wander/static/media/kirokaze.8db746ea.gif"
+  },
+  {
+    "revision": "9470e17c56c0df40257cf7c2c392acc7",
+    "url": "/wander/static/media/map-min.9470e17c.png"
+  }
+]);
