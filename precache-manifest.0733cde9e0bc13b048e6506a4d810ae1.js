9d7c1546e12dd141bc9dcc9edff73e3ef058ed31
Al Yin <altyin@gmail.com>
2020-03-10 20:25:18 -0400

    Updates

---
+++ precache-manifest.0733cde9e0bc13b048e6506a4d810ae1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1adfb1a39d9bc0578d32b2ac8509f0a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "913eae143c0c25270a62",
+    "url": "/static/css/main.dfd7e88a.chunk.css"
+  },
+  {
+    "revision": "9e0c8d8badcdc089c5b2",
+    "url": "/static/js/2.1d1f43dd.chunk.js"
+  },
+  {
+    "revision": "7f3d623d03408f9350c3b5e194511d01",
+    "url": "/static/js/2.1d1f43dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913eae143c0c25270a62",
+    "url": "/static/js/main.a26a7175.chunk.js"
+  },
+  {
+    "revision": "622aa263e11fb0798bda",
+    "url": "/static/js/runtime-main.9048781c.js"
+  },
+  {
+    "revision": "8db746ea0f28bdd22eae8b3ee10ac16a",
+    "url": "/static/media/kirokaze.8db746ea.gif"
+  },
+  {
+    "revision": "9470e17c56c0df40257cf7c2c392acc7",
+    "url": "/static/media/map-min.9470e17c.png"
+  }
+]);
